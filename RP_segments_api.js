46f0a15f269980075713d2b09c801caacca07ed1
Marcin Fortuna <marcin.fort@gmail.com>
2018-03-20 13:09:51 +0100

    Add all questions on consonants

--- RP_segments_api.js
+++ RP_segments_api.js
@@ -507,6 +507,211 @@ export const questions = [
     {
         question: "a labiovelar consonant",
         classes: ["labiovelar"]
+    },
+
+    {
+        question: "a voiced plosive",
+        classes: ["b", "d", "g"]
+    },
+
+    {
+        question: "a voiceless plosive",
+        classes: ["p", "t", "k"]
+    },
+
+    {
+        question: "a voiced fricative",
+        classes: ["v", "D", "z", "Z"]
+    },
+
+    {
+        question: "a voiceless fricative",
+        classes: ["f", "T", "s", "S", "h"]
+    },
+
+    {
+        question: "a voiced affricate",
+        classes: ["dZ"]
+    },
+
+    {
+        question: "a voiceless affricate",
+        classes: ["tS"]
+    },
+
+    {
+        question: "a bilabial plosive",
+        classes: ["p", "b"]
+    },
+
+    {
+        question: "a labiodental fricative",
+        classes: ["v", "f"]
+    },
+
+    {
+        question: "a dental fricative",
+        classes: ["T", "D"]
+    },
+
+    {
+        question: "an alveolar fricative",
+        classes: ["s", "z"]
+    },
+
+    {
+        question: "an alveolar plosive",
+        classes: ["t", "d"]
+    },
+
+    {
+        question: "an alveolar sonorant",
+        classes: ["l", "n"]
+    },
+
+    {
+        question: "an alveolar obstruent",
+        classes: ["t", "d", "s", "z"]
+    },
+
+    {
+        question: "an alveolar approximant",
+        classes: ["l"]
+    },
+
+    {
+        question: "an alveolar nasal",
+        classes: ["n"]
+    },
+
+    {
+        question: "a postalveolar approximant",
+        classes: ["r"]
+    },
+
+    {
+        question: "a palato-alveolar fricative",
+        classes: ["S", "Z"]
+    },
+
+    {
+        question: "a palato-alveolar affricate",
+        classes: ["tS", "dZ"]
+    },
+
+    {
+        question: "a velar plosive",
+        classes: ["k", "g"]
+    },
+
+    {
+        question: "a velar nasal",
+        classes: ["N"]
+    },
+
+    {
+        question: "a glottal fricative",
+        classes: ["h"]
+    },
+
+    {
+        question: "a voiceless bilabial plosive",
+        classes: ["p"]
+    },
+
+    {
+        question: "a voiced bilabial plosive",
+        classes: ["b"]
+    },
+
+    {
+        question: "a voiceless labiodental fricative",
+        classes: ["f"]
+    },
+
+    {
+        question: "a voiced labiodental fricative",
+        classes: ["v"]
+    },
+
+    {
+        question: "a voiceless dental fricative",
+        classes: ["T"]
+    },
+
+    {
+        question: "a voiced dental fricative",
+        classes: ["D"]
+    },
+
+    {
+        question: "a voiceless alveolar plosive",
+        classes: ["t"]
+    },
+
+    {
+        question: "a voiced alveolar plosive",
+        classes: ["d"]
+    },
+
+    {
+        question: "a voiceless alveolar fricative",
+        classes: ["s"]
+    },
+
+    {
+        question: "a voiced alveolar fricative",
+        classes: ["z"]
+    },
+
+    {
+        question: "a voiced palato-alveolar fricative",
+        classes: ["Z"]
+    },
+
+    {
+        question: "a voiceless palato-alveolar fricative",
+        classes: ["S"]
+    },
+
+    {
+        question: "a voiced palato-alveolar affricate",
+        classes: ["dZ"]
+    },
+
+    {
+        question: "a voiceless palato-alveolar affricate",
+        classes: ["tS"]
+    },
+
+    {
+        question: "a palatal approximant",
+        classes: ["j"]
+    },
+
+    {
+        question: "a voiced velar plosive",
+        classes: ["g"]
+    },
+
+    {
+        question: "a voiceless velar plosive",
+        classes: ["k"]
+    },
+
+    {
+        question: "a labiovelar approximant",
+        classes: ["w"]
+    },
+
+    {
+        question: "a liquid",
+        classes: ["l", "r"]
+    },
+
+    {
+        question: "a semi-vowel",
+        classes: ["j", "w"]
     }
 
 ]
